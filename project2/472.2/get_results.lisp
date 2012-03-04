144c33ff9ce02ed7bb4b9754352f4211a8fbaaa2
Steven White <swhite24@mix.wvu.edu>
2012-03-03 20:20:39 -0500

    runtimes more readable

--- project2/472.2/get_results.lisp
+++ project2/472.2/get_results.lisp
@@ -3,7 +3,8 @@
 (format t "~4:@<~a~>~6:@<~a~>~11:@<~a~>~11:@<~a~>~11:@<~a~>~11:@<~a~>~%"
 	'b 'n 'dfs 'dfid_dfs 'beam 'dfid_beam)
 
-(let ((b_vals '(2 4 8 16 24 48))
+(let ((start_time (get-internal-run-time))
+      (b_vals '(2 4 8 16 24 48))
       (n_vals '(40 80 160 325 650 1250 2500 5000)))
   (dolist (b b_vals)
     (dolist (n n_vals)
@@ -13,4 +14,7 @@
       (time-of-1000 (!test_dfid_dfs n b))
       (time-of-1000 (!test_beam n b))
       (time-of-1000 (!test_dfid_beam n b))
-      (format t "~%"))))
+      (format t "~%")))
+  (format t "~%~%Total time for all executions: ~f~%"
+	  (/ (- (get-internal-run-time) start_time) 
+	     internal-time-units-per-second)))
